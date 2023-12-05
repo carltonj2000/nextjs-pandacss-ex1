432b4e56a238570db393ab3bb060803681e2afcf
Carlton Joseph <carlton.joseph@gmail.com>
2023-12-05 12:11:34 -0800

    add button recipe

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,41 +5,73 @@ import { center, circle, hstack, stack } from "../../styled-system/patterns";
 const buttonRecipe = cva({
   base: {
     cursor: "pointer",
+    background: "yellow.400",
+    rounded: "7px",
+  },
+  variants: {
+    visual: {
+      solid: { bg: "red.500", color: "white" },
+      outline: { borderWidth: "1px", borderColor: "red.900" },
+    },
+    size: {
+      sm: { padding: "4", fontSize: "12px" },
+      lg: { padding: "8", fontSize: "24px" },
+    },
+  },
+  compoundVariants: [
+    {
+      visual: "solid",
+      size: "sm",
+      css: { borderWidth: "4px", borderColor: "black" },
+    },
+  ],
+  defaultVariants: {
+    visual: "solid",
+    size: "lg",
   },
 });
 
 export default function Home() {
   return (
     <div className={center({ height: "100vh" })}>
-      <div
-        className={css({
-          border: "3px solid #000000",
-          boxShadow: "4px 4px 0px #000000",
-          borderRadius: "13px",
-          padding: 6,
-          maxWidth: "388px",
-        })}
-      >
-        <div className={stack({ gap: 4 })}>
-          <div className={hstack({ gap: 4 })}>
-            <div className={circle({ size: 12, overflow: "hidden" })}>
-              <Image
-                src="https://avatars.githubusercontent.com/u/834149?v=4"
-                alt="Carlton Joseph"
-                height={120}
-                width={120}
-              />
+      <div className={stack({ gap: 2 })}>
+        <button className={buttonRecipe({ visual: "outline", size: "lg" })}>
+          hi
+        </button>
+        <button className={buttonRecipe({})}>hello</button>
+        <button className={buttonRecipe({ visual: "solid", size: "sm" })}>
+          hello
+        </button>
+        <div
+          className={css({
+            border: "3px solid #000000",
+            boxShadow: "4px 4px 0px #000000",
+            borderRadius: "13px",
+            padding: 6,
+            maxWidth: "388px",
+          })}
+        >
+          <div className={stack({ gap: 4 })}>
+            <div className={hstack({ gap: 4 })}>
+              <div className={circle({ size: 12, overflow: "hidden" })}>
+                <Image
+                  src="https://avatars.githubusercontent.com/u/834149?v=4"
+                  alt="Carlton Joseph"
+                  height={120}
+                  width={120}
+                />
+              </div>
+              <div>
+                <h2 className={css({ fontWeight: "600" })}>Carlton Joseph</h2>
+                <p className={css({ color: "gray.400" })}>@carlton_joseph</p>
+              </div>
             </div>
-            <div>
-              <h2 className={css({ fontWeight: "600" })}>Carlton Joseph</h2>
-              <p className={css({ color: "gray.400" })}>@carlton_joseph</p>
-            </div>
+            <blockquote className={css({ fontSize: "17px" })}>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+              animi inventore fugiat perspiciatis nulla suscipit! Eaque tempora
+              praesentium tempore similique!
+            </blockquote>
           </div>
-          <blockquote className={css({ fontSize: "17px" })}>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
-            animi inventore fugiat perspiciatis nulla suscipit! Eaque tempora
-            praesentium tempore similique!
-          </blockquote>
         </div>
       </div>
     </div>
