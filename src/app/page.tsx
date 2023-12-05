bc8d9e0d76e30efd4ef5e884cc1952f9955f1c4f
Carlton Joseph <carlton.joseph@gmail.com>
2023-12-05 10:24:41 -0800

    add card component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,39 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import { css } from "../../styled-system/css";
+import { center, circle, hstack, stack } from "../../styled-system/patterns";
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className={center({ height: "100vh" })}>
+      <div
+        className={css({
+          border: "3px solid #000000",
+          boxShadow: "4px 4px 0px #000000",
+          borderRadius: "13px",
+          padding: 6,
+          maxWidth: "388px",
+        })}
+      >
+        <div className={stack({ gap: 4 })}>
+          <div className={hstack({ gap: 4 })}>
+            <div className={circle({ size: 12, overflow: "hidden" })}>
+              <img
+                src="https://avatars.githubusercontent.com/u/834149?v=4"
+                alt="Carlton Joseph"
+                height="120px"
+                width="120px"
+              />
+            </div>
+            <div>
+              <h2 className={css({ fontWeight: "600" })}>Carlton Joseph</h2>
+              <p className={css({ color: "gray.400" })}>@carlton_joseph</p>
+            </div>
+          </div>
+          <blockquote className={css({ fontSize: "17px" })}>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam
+            animi inventore fugiat perspiciatis nulla suscipit! Eaque tempora
+            praesentium tempore similique!
+          </blockquote>
         </div>
       </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
